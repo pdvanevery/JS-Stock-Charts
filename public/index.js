cb7124141a86825d7b96f583ba3f32cc0889eb6c
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-02-09 17:53:53 -0800

    did the first few steps

--- public/index.js
+++ public/index.js
@@ -4,6 +4,15 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=41cd5acb82324c11a7f507078ef26dea`)
+
+    const result = response.json()
+
+    const { GME, MSFT, DIS, BNTX } = result;
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    stocks.forEach( stock => stock.values.reverse())
 }
 
 main()
